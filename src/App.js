345a2cf5a1f951ad47a65e15d102dc9aa63ac7c0
Scotty <scottmtinez@gmail.com>
2024-08-22 17:21:31 -0500

    First Push

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from 'react';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'>[PUMP]</header>
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>HOME</a>
+          </li>
+          <li>
+            <a href='/'>MY WORKOUT</a>
+          </li>
+          <li>
+            <a href='/'>COMMUNITY</a>
+          </li>
+          <li>
+            <a href='/'>EXERCISES</a>
+          </li>
+          <li>
+            <a href='/'>ACCOUNT</a>
+          </li>
+        </ul>
+      </nav>
+
+      <Home />
+      
+      <footer className='footer'>Workout Tracker Web Application | Portfolio Project | 2024</footer>
+    </div>
+  );
+}
+
+export default App;
