3b5797ebf35eafb55afd095e77ee908ec47c5f1f
Scotty <scottmtinez@gmail.com>
2024-08-23 21:14:48 -0500

    Updated Acount.js

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,9 +4,19 @@ import './Account.css';
 function Account(){
     return (
         <div className='Account-container'>
-            <div className=''>
-                Account Page
-            </div>
+            <form className='Account-form-login'>
+                <h1 className=''>LOGIN</h1>
+                <input type='text' placeholder='Username...' />
+                <input type='password' placeholder='Password...'/>
+                <input type='submit' value='Login'/>
+            </form>
+
+            <form className='Account-form-signup'>
+                <h1 className=''>SIGN UP</h1>
+                <input type='text' placeholder='Username...' />
+                <input type='password' placeholder='Password...'/>
+                <input type='submit' value='Signup'/>
+            </form>
         </div>
     );
 }
