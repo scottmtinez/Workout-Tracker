11416be20b05c4a04037676708a35965fb492479
Scott Martínez <scottmtinez@gmail.com>
2025-01-08 21:22:59 -0600

    Update Exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './Exercises.css';
 
-function Exercises(){
+const Exercises = () => {
+    //States
+        const [user, setUser] = useState(null);
+    
+      // Retrieve user data from localStorage when the component mounts
+        useEffect(() => {
+          const storedUser = localStorage.getItem('user');
+    
+          if (storedUser) {
+            setUser(JSON.parse(storedUser));
+            console.log('User data retrieved from localStorage:', storedUser); // For Testing
+          }
+        }, []); // Runs only once when the component mounts
+
     return (
         <div className='Excersises-container'>
             <div className=''>
                 Exercises Page
-                <div class name=''>
-        
-               </div>
             </div>
         </div>
     );
