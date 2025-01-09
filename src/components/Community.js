3fbc5b4ac610d0e6d10ca590424f8955276e57a7
Scott Martínez <scottmtinez@gmail.com>
2025-01-08 21:21:13 -0600

    Update Community.js

--- src/components/Community.js
+++ src/components/Community.js
@@ -2,7 +2,19 @@ import React, { useState, useEffect } from 'react';
 import './Community.css';
 
 function Community() {
-  const [leaderboard, setLeaderboard] = useState([]);
+  //States
+    const [leaderboard, setLeaderboard] = useState([]);
+    const [user, setUser] = useState(null);
+
+  // Retrieve user data from localStorage when the component mounts
+    useEffect(() => {
+      const storedUser = localStorage.getItem('user');
+
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+        console.log('User data retrieved from localStorage:', storedUser); // For Testing
+      }
+    }, []); // Runs only once when the component mounts
 
   useEffect(() => {
     // Simulate fetching leaderboard data from an API or database
