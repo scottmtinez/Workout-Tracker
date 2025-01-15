476cebc292dfee18acb7ac9c680d1563d0c0c26f
Scott Martínez <scottmtinez@gmail.com>
2025-01-14 21:00:29 -0600

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,2 +1,25 @@
-// --- Dashboard.js --- 
-//Note: Only for the admin user to view
+import React, { useState, useEffect } from 'react';
+import './Dashboard.css';
+
+const Dashboard = () => {
+    //States
+        const [user, setUser] = useState(null);
+    
+      // Retrieve user data from localStorage when the component mounts
+        useEffect(() => {
+          const storedUser = localStorage.getItem('user');
+    
+          if (storedUser) {
+            setUser(JSON.parse(storedUser));
+            console.log('User data retrieved from localStorage:', storedUser); // For Testing
+          }
+        }, []); // Runs only once when the component mounts
+
+    return (
+        <div className='Dashboard-container'>
+            
+        </div>
+    );
+}
+
+export default Dashboard;
