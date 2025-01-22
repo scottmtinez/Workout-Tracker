059574164d13507b0a94dc4733c045adc04cdc88
Scott Martínez <scottmtinez@gmail.com>
2025-01-21 21:08:29 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
             if (storedUser) {
                 setUser(JSON.parse(storedUser));
-                //console.log('User data retrieved from localStorage:', storedUser); // For Testing
+                console.log('User data retrieved from localStorage:', storedUser); // For Testing
             }
         }, []); // Runs only once when the component mounts
 
@@ -25,18 +25,23 @@ const Home = () => {
                 <div className='home-exercies-tab-container'>
                     <div className='home-exercies1-plan'>
                         <span className='home-exercise-sub-title'>POWERLIFTING WORKOUT</span>
+                        <span className='home-exercise-price'>$19.99</span>
                     </div>
                     <div className='home-exercies2-plan'>
                         <span className='home-exercise-sub-title'>BODYBUILDING WORKOUT</span>
+                        <span className='home-exercise-price'>$19.99</span>
                     </div>
                     <div className='home-exercies3-plan'>
                         <span className='home-exercise-sub-title'>GROUP RUN PLAN</span>
+                        <span className='home-exercise-price'>$9.99</span>
                     </div>
                     <div className='home-exercies4-plan'>
-                        <span className='home-exercise-sub-title'>LEG DAY</span>
+                        <span className='home-exercise-sub-title'>LEG DAY PLAN</span>
+                        <span className='home-exercise-price'>$19.99</span>
                     </div>
                     <div className='home-exercies5-plan'>
                         <span className='home-exercise-sub-title'>MORNING YOGA PLAN</span>
+                        <span className='home-exercise-price'>$14.99</span>
                     </div>
                 </div>
                 <div className='home-x-container'>
