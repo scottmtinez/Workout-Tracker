9011437efc8f614166ce64e86da327f9773b8caa
Scott Martínez <scottmtinez@gmail.com>
2025-02-07 19:43:51 -0600

    Update Community.js

--- src/components/Community.js
+++ src/components/Community.js
@@ -156,7 +156,11 @@ function Community() {
                 placeholder='Write a comment...'
               />
               <button className='Community-comment-btn' onClick={() => handleCommentSubmit(post._id)}><i class="bi bi-chat-left-text"></i></button>
-              <button className='Community-delete-btn' onClick={() => handleDeletePost(post._id)}><i class="bi bi-trash3"></i></button>
+              {user?.username === post.username && (
+                <button className='Community-delete-btn' onClick={() => handleDeletePost(post._id)}>
+                  <i className="bi bi-trash3"></i>
+                </button>
+              )}
             </div>
           ))
         ) : (
